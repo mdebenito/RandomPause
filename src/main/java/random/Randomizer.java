99255d8fa37277fcfa432b67575a85bba230ce6a
mdebenito <mdebenito@users.noreply.github.com>
2017-04-04 11:29:58 +0200

    Update Randomizer.java

--- src/main/java/random/Randomizer.java
+++ src/main/java/random/Randomizer.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 /**
  * Created by Mario de Benito on 04/04/2017.
+ * Used code on this thread:
+ * http://stackoverflow.com/questions/363681/how-to-generate-random-integers-within-a-specific-range-in-java
  */
 public class Randomizer {
 
@@ -17,15 +19,8 @@ public class Randomizer {
      * @return Integer between min and max, inclusive.
      * @see java.util.Random#nextInt(int)
      */
-    public static int randInt(int min, int max) {
-        // NOTE: This will (intentionally) not run as written so that folks
-        // copy-pasting have to think about how to initialize their
-        // Random instance.  Initialization of the Random instance is outside
-        // the main scope of the question, but some decent options are to have
-        // a field that is initialized once and then re-used as needed or to
-        // use ThreadLocalRandom (if using at least Java 1.7).
+    public static int randInt(int min, int max) {        
         Random rand = new Random();
-
         // nextInt is normally exclusive of the top value,
         // so add 1 to make it inclusive
         int randomNum = rand.nextInt((max - min) + 1) + min;
